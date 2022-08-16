69166b41f752b5ad755449f57d253dcdcaa24bc1
YuvrajSingh-kellton <105622006+YuvrajSingh-kellton@users.noreply.github.com>
2022-08-17 04:06:23 +0530

    Initial Commit

---
+++ EntityFrameworkConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using EntityFramework.Data;
+using EntityFramework.Data.Entities;
+
+
+public class program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Insert Employee");
+
+        CRUDManager obj = new CRUDManager();
+        Console.WriteLine("Enter EMP");
+        var naam = Console.ReadLine();
+        var addr = Console.ReadLine();
+        obj.Insert(new Employee { Name = naam, Address = addr });
+        PrintAllEmployees();
+        
+    }
+
+    private static void PrintAllEmployees()
+    {
+        Console.WriteLine("Printing All Employees");
+        CRUDManager obj = new CRUDManager();
+        foreach(Employee employee in obj.GetAllEmployees())
+        {
+            Console.WriteLine($"Employee Name is {employee.Name} and address is {employee.Address}");
+        }
+    }
+}
