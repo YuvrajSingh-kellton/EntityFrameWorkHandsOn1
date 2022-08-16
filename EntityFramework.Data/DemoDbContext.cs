b764a55f22c6163292a463ffb88d4301f74e8853
YuvrajSingh-kellton <105622006+YuvrajSingh-kellton@users.noreply.github.com>
2022-08-17 03:41:16 +0530

    Initial Commit

---
+++ EntityFramework.Data/DemoDbContext.cs
@@ -0,0 +1,26 @@
+﻿using EntityFramework.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework.Data
+{
+    public class DemoDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<EmployeeEducation> EmployeeEducations { get; set; }
+
+        public DemoDbContext()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-0P56O06;Database=EFDatabase;Trusted_Connection=True;");
+
+        }
+    }
+}
